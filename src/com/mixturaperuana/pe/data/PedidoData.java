41c48cf59a7e807cd3e66fd01fef0699d068bc04
Luis Alejandro Ayala Magni <voltluis220@gmail.com>
2024-09-25 21:30:58 -0500

    Añadido de Pedido de Datos

--- src/com/mixturaperuana/pe/data/PedidoData.java
+++ src/com/mixturaperuana/pe/data/PedidoData.java
@@ -1,4 +1,48 @@
 package com.mixturaperuana.pe.data;
 
 public class PedidoData {
+    private int idPedido;
+    private Cliente cliente;
+    private List<Plato> platos;
+    private EstadoPedido estado;
+    private Empleado registradoPor;
+    private boolean paraLlevar;
+    private String direccion;
+    private int tiempoEntrega;
+
+    public PedidoData(int idPedido, Cliente cliente, List<Plato> platos, EstadoPedido estado, Empleado registradoPor, boolean paraLlevar, String direccion, int tiempoEntrega) {
+        this.idPedido = idPedido;
+        this.cliente = cliente;
+        this.platos = platos;
+        this.estado = estado;
+        this.registradoPor = registradoPor;
+        this.paraLlevar = paraLlevar;
+        this.direccion = direccion;
+        this.tiempoEntrega = tiempoEntrega;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public EstadoPedido getEstado() {
+        return estado;
+    }
+    public List<Plato> getPlatos() {
+        return platos;
+    }
+    public Empleado getRegistradoPor() {
+        return registradoPor;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public int getTiempoEntrega() {
+        return tiempoEntrega;
+    }
+    public boolean isParaLlevar() {
+        return paraLlevar;
+    }
 }
---
+++ src/com/mixturaperuana/pe/enums/Rol.java
@@ -0,0 +1,7 @@
+package com.mixturaperuana.pe.enums;
+
+public enum Rol {
+    MESERO,
+    ASISTENTE_COCINA,
+    ADMINISTRADOR
+}
