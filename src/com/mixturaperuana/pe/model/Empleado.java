d2f9e39e402355a7d7c563037f86757e51a43d8b
nancy lopez raygada <agatalopezraygada@gmail.com>
2024-09-25 21:47:37 -0500

    Creación de Clase Empleado con extensión de la clase Usuario, atributos y metodos

--- src/com/mixturaperuana/pe/model/Empleado.java
+++ src/com/mixturaperuana/pe/model/Empleado.java
@@ -1,4 +1,28 @@
 package com.mixturaperuana.pe.model;
 
-public abstract class Empleado {
+import com.mixturaperuana.pe.enums.Rol;
+
+public abstract class Empleado extends Usuario {
+    private int idEmpleado;
+    private Rol rol;
+    private double salario;
+
+    public Empleado(String usuario, String contrasena, int idEmpleado, Rol rol, double salario) {
+        super(usuario, contrasena);
+        this.idEmpleado = idEmpleado;
+        this.rol = rol;
+        this.salario = salario;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public Rol getRol() {
+        return rol;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
 }
